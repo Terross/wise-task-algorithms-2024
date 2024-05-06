41ed7ae2775812b9a779356f2b977a283d51fdb2
jqnfxa <ivan.boriso2014@gmail.com>
2024-05-06 19:43:38 +0300

    IsPlanarWithoutSomeEdge

---
+++ carpe-diem/IsPlanarWithoutSomeEdge/src/main/java/Pair.java
@@ -0,0 +1,10 @@
+public record Pair<T, U>(T first, U second) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return first.equals(pair.first) && second.equals(pair.second);
+    }
+
+}
