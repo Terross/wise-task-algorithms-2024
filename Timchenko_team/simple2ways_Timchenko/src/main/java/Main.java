ec2f12462608503b86adbf357b359c898eaa2df8
Dmitry Timchenko <112801926+Alisha192@users.noreply.github.com>
2024-05-25 18:07:53 +0300

    Timchenko
    Добавлена модицикация кода: теперь работает с любым типом графа

--- Timchenko_team/simple2ways_Timchenko/src/main/java/Main.java
+++ Timchenko_team/simple2ways_Timchenko/src/main/java/Main.java
@@ -6,7 +6,7 @@ import java.io.FileNotFoundException;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         Simple2ways Simple2ways = new Simple2ways();
-        var testGraph = GraphFactory.loadGraphFromFile(new File("src/main/resources/graph_false_two_strong_connection.txt"));
+        var testGraph = GraphFactory.loadGraphFromFile(new File("C:/Users/gidra/IdeaProjects/untitled7/src/main/resources/graph_false.txt"));
         boolean result = Simple2ways.execute(testGraph);
         System.out.println(result);
     }
