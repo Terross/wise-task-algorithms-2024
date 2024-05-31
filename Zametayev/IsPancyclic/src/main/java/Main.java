2e99bdee94e2b7d34ad9df668c9ad5ff8efd910c
ascens1ve <silensteam@mail.ru>
2024-06-01 00:15:49 +0300

    Add 2 modules

---
+++ Zametayev/IsPancyclic/src/main/java/Main.java
@@ -0,0 +1,13 @@
+import com.mathsystem.api.graph.GraphFactory;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        var testg = GraphFactory.loadGraphFromFile(new File("C:\\Education\\Programming\\Collocvium\\IsPancyclic\\src\\main\\resources\\k5.txt"));
+        IsPancyclic pan = new IsPancyclic();
+        boolean pan_int = pan.execute(testg);
+        System.out.println(pan_int);
+    }
+}
