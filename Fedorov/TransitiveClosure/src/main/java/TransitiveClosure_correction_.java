496a901984599fbca24253e9c6f2949c3c67d55d
mishIgr <fmikhailv@gmail.com>
2024-06-02 18:10:46 +0300

    new_change

--- Fedorov/TransitiveClosure/src/main/java/TransitiveClosure_correction_.java
+++ Fedorov/TransitiveClosure/src/main/java/TransitiveClosure_correction_.java
@@ -1,14 +1,14 @@
 import com.mathsystem.api.graph.model.Graph;
 import com.mathsystem.domain.plugin.plugintype.GraphProperty;
 
-public class TransitiveClosure implements GraphProperty {
+public class TransitiveClosure_correction_ implements GraphProperty {
 
     public AdjacencyMatrix transitiveClosure(final AdjacencyMatrix adjacency_matrix) {
         int[][] matrix = adjacency_matrix.getMatrix();
 
         for (int k = 0; k < matrix.length; ++k)
             for (int i = 0; i < matrix.length; ++i)
-                for (int j = 0; j < matrix.length; ++j)
+                for (int j = 0; j < matrix.length && i != j; ++j)
                     matrix[i][j] = matrix[i][j] | (matrix[i][k] & matrix[k][j]);
 
         AdjacencyMatrix transitive_closure_matrix = new AdjacencyMatrix(adjacency_matrix);
