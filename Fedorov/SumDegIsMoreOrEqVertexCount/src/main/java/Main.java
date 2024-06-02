9a2701cd19859f6b174f606822a5936a86db810c
mishIgr <fmikhailv@gmail.com>
2024-06-02 18:38:10 +0300

    add_SumDegIsMoreOrEqVertexCount

---
+++ Fedorov/SumDegIsMoreOrEqVertexCount/src/main/java/Main.java
@@ -0,0 +1,13 @@
+import com.mathsystem.api.graph.GraphFactory;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        SumDegIsMoreOrEqVertexCount chromaticNumber2 = new SumDegIsMoreOrEqVertexCount();
+        var testGraph = GraphFactory.loadGraphFromFile(new File("src/main/resources/graph1.txt"));
+        boolean result = chromaticNumber2.execute(testGraph);
+        System.out.println(result);
+    }
+}
