4c438986a249fa7d16dcc8562a0c6f4f554ca064
googgom <matvey.volodchenko@gmail.com>
2024-06-03 18:56:56 +0300

    GOGO

--- Volodchenko/src/main/java/CoveringVertexSet.java
+++ Volodchenko/src/main/java/CoveringVertexSet.java
@@ -6,7 +6,7 @@ import com.mathsystem.domain.plugin.plugintype.GraphCharacteristic;
 import java.util.*;
 
 public class CoveringVertexSet implements GraphCharacteristic {
-    public Map<Vertex, List<Edge>> getVerticesEdges(Graph graph) {
+    public Map<Vertex, List<Edge>> getVerticesEdges(Graph graph) {///делает Map списка рёбер по вершинам
         Map<Vertex, List<Edge>> res = new HashMap<>();
         for (Vertex vertex : graph.getVertices().values()) {
             res.put(vertex, new ArrayList<>());
@@ -24,21 +24,19 @@ public class CoveringVertexSet implements GraphCharacteristic {
         int vcnt = graph.getVertexCount();
         int ecnt = graph.getEdgeCount();
         int ans = vcnt;
-        int limit = 1;
-        int len = 0;
+        int limit = 1;///2^vcnt
+        int len = 0;///....вообще равен vcnt.....но имеет духовную ценность
         for( int i = 0; i < vcnt; ++ i ){
             limit *= 2;
             len ++;
         }
-//        System.out.println(len);
-//        System.out.println(limit);
 
-        Map<Vertex, List<Edge>> map = new HashMap<Vertex, List<Edge>>(getVerticesEdges(graph));
-        List<Edge> edges = new ArrayList<Edge>(graph.getEdges());
-        List<Vertex> vertices = new ArrayList<Vertex>(graph.getVertices().values().stream().toList());
 
-        Set<Edge> total = new HashSet<Edge>();
-        //System.out.println(Arrays.toString(vertices.toArray()));
+        Map<Vertex, List<Edge>> map = new HashMap<Vertex, List<Edge>>(getVerticesEdges(graph));///Удобный доступ к рёбрам конкретных вершин
+        List<Edge> edges = new ArrayList<Edge>(graph.getEdges());///Все вершины, для "Удобный доступ"
+        List<Vertex> vertices = new ArrayList<Vertex>(graph.getVertices().values().stream().toList());///Все рёбра
+
+        Set<Edge> total = new HashSet<Edge>();///Все рёбра в 1 месте, для быстрого сравнения структур
         for( Edge e: edges ){
             total.add(e);
         }
@@ -48,37 +46,32 @@ public class CoveringVertexSet implements GraphCharacteristic {
             int[] tmp = new int[len+1];
             int copy = i;
             int ind = 0;
-            while( copy != 0 ){
+            while( copy != 0 ){///Этот сегмент преобразует число в список используемых сейчас элементов
                 tmp[ind] = copy % 2;
                 copy /= 2;
                 ind += 1;
             }
-            Set<Edge> used = new HashSet<Edge>();
-//            System.out.println(i);
-//            for( int j = 0; j < len + 1; ++ j ){
-//                System.out.println(tmp[j]);
-//            }
-//            System.out.println("\n-------\n");
+
+
+            Set<Edge> used = new HashSet<Edge>();///Собираем сюда рёбра, что бы сравнить с total и понять, достигли ли мы цели
 
 
             int newans = 0;
-            for( int j = 0; j < len; ++ j ){
-                if( tmp[j] == 1 ){
-                    newans ++;
-                    for( Edge e: map.get(vertices.get(j)) ){
-                        used.add(e);
+            for( int j = 0; j < len; ++ j ){///По всем вершинам
+                if( tmp[j] == 1 ){///Но интересуют только "разрешённые" на этом ходу
+                    newans ++;///Потенциально взяли вершину
+                    for( Edge e: map.get(vertices.get(j)) ){///Нужно учесть рёбра, что бы потом быстро сравнить сеты и понять
+                        used.add(e);///Достигнута ли цель
                     }
                 }
             }
 
-//            System.out.println(Arrays.toString(used.toArray()));
-//            System.out.println(Arrays.toString(total.toArray()));
-//            System.out.println(newans);
-            if( !used.equals(total) ){
+
+            if( !used.equals(total) ){///Стираем потенциальный ответ, если цель не была достигнута
                 newans = ans;
             }
 
-            if( newans < ans ) ans = newans;
+            if( newans < ans ) ans = newans;///Если вышло лучше чем было...то нужно запомнить
         }
 
         return ans;
